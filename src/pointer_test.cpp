8674a68ff0cdcc112a19ffcf3f33d3d4f6bf47c2
Adnan Munawar <amunawar@wpi.edu>
2017-10-24 02:22:15 -0400

    Best workout is using the boost::function and boost::bind for callbacks

--- src/pointer_test.cpp
+++ src/pointer_test.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <stdio.h>
+#include <functional>
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+
 class B;
 
 template <typename U, typename V>
@@ -8,6 +12,12 @@ public:
   Conversion(){}
   ~Conversion(){}
   void (U::*my_func)(V);
+  void trythis(const boost::function<void (V)> cal_fcn){
+    printf("2nd Pointer function in A called\n");
+    cal_fcn(33);
+
+  }
+
 };
 
 class A{
@@ -19,13 +29,17 @@ public:
    }
    void (B::*fcn2)(int);
    template <typename T, typename P>
-   void init(void (T::*b_func)(P), T &obj){
+   void init(void (T::*b_func)(P), T *obj){
      printf("Pointer function in A called \n");
-     Conversion<T,P> con;
-     con.my_func = b_func;
-     fcn2 = b_func;
-     (obj.*fcn2)(15);
-     (obj.*(con.my_func))(2);
+    Conversion<T,P> con;
+    con.my_func = b_func;
+    fcn2 = b_func;
+    (obj->*fcn2)(15);
+    (obj->*(con.my_func))(2);
+
+    con.trythis(boost::bind(b_func, obj, _1));
+   }
+   void run(){
    }
 };
 
@@ -45,7 +59,7 @@ public:
   }
 
   void run(){
-    init(&B::B_func, *this);
+    init(&B::B_func, this);
   }
 private:
   int b_val = 2;
