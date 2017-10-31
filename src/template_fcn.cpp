bae0e9298925dad8b89ed5ff5d1f03ef93198eb1
Adnan Munawar <amunawar@wpi.edu>
2017-10-31 00:47:01 -0400

    Calling the conv_fcns using proper syntax and how to use them

--- src/template_fcn.cpp
+++ src/template_fcn.cpp
@@ -15,12 +15,19 @@ public:
   boost::function<void (D)> my_func;
 };
 
-class B: public A<int>, public A<char>{
+class B: public A<int>, public A<double>, public A<std::string>{
 public:
-  B(){}
+  B(){
+    my_int = 1;
+    my_str = "Hello World";
+  }
   ~B(){}
+  std::string my_str;
+  int my_int;
   void run(){
-    
+    A<int>::my_func(my_int);
+    printf("Pause\n");
+    A<std::string>::my_func(my_str);
   }
 };
 
@@ -29,23 +36,36 @@ public:
   C(){}
   ~C(){}
   void int_func(int x){
-    printf("Printing passed char %d", x);
+    printf("Printing passed int %d\n", x);
   }
-  void char_func(char x){
-    printf("Printing passed char %c", x);
+  void double_func(double x){
+    printf("Printing passed int %f\n", x);
+  }
+  void str_func(std::string x){
+    printf("Printing passed char %s\n", x.c_str());
   }
   void assign_int_func(){
   A<int>::bind_func(&C::int_func, this);
   }
+  void assign_double_func(){
+  A<double>::bind_func(&C::double_func, this);
+  }
   void assign_char_func(){
-  A<char>::bind_func(&C::char_func, this);
+  A<std::string>::bind_func(&C::str_func, this);
+  }
+
+  void assign_fcns(){
+    assign_int_func();
+    assign_char_func();
+    assign_double_func();
   }
 };
 
 
 int main(){
   C cobj;
-  cobj.assign_int_func();
+  cobj.assign_fcns();
+  cobj.run();
   //cobj.assign_char_func();
 
   return 0;
